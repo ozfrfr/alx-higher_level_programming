df1bb9fb70d498f2df04edf7697af08baa384dfd
hosanna57 <hosannacchigbu@gmail.com>
2023-10-06 02:34:22 +0100

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 from add_0 import add
-
 a = 1
 b = 2
 result = add(a, b)
@@ -8,4 +7,4 @@ formatted_string = "{} + {} = {}".format(a, b, result)
 print(formatted_string)
 
 if __name__ == "__main__":
-    pass
+pass
