5442355e948ee246cf332c4bbc1b927f600a85c3
hosanna57 <hosannacchigbu@gmail.com>
2023-10-06 04:36:40 +0100

    arg

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+
+# Get the number of command-line arguments (including the script name)
+num_args = len(sys.argv) - 1  # Subtract 1 for the script name itself
+
+if num_args == 0:
+    print("0 arguments.")
+elif num_args == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(num_args))
+
+for i, arg in enumerate(sys.argv[1:], start=1):
+    print("{}: {}".format(i, arg))
+
+
+
+
+
