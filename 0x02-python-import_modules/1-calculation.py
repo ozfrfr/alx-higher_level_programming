4af7beef1a0ea11c6ac9450c15c53f8241373ade
hosanna57 <hosannacchigbu@gmail.com>
2023-10-06 03:56:28 +0100

    toolbox

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+result = add(a, b)
+formatted_string = "{} + {} = {}".format(a, b, result)
+print(formatted_string)
+
+a = 10
+b = 5
+result = sub(a, b)
+formatted_string = "{} - {} = {}".format(a, b, result)
+print(formatted_string)
+
+a = 10
+b = 5
+result = mul(a, b)
+formatted_string = "{} * {} = {}".format(a, b, result)
+print(formatted_string)
+
+a = 10
+b = 5
+result = div(a, b)
+formatted_string = "{} / {} = {}".format(a, b, result)
+print(formatted_string)
+
+if __name__ == "__main__":
+    pass
