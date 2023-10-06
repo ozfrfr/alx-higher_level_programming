d9d5ef6e99366224226bf4773f86fd9c9a4645e0
ozfrfr <hosannacchigbu@gmail.com>
2023-10-05 21:00:01 -0700

    infinite_add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+
+# Initialize a variable to store the sum of arguments
+sum_of_arguments = 0
+
+# Loop through command-line arguments starting from the second one
+for arg in sys.argv[1:]:
+    # Convert the argument to an integer and add it to the sum
+    sum_of_arguments += int(arg)
+
+# Print the sum of all arguments
+print(sum_of_arguments)
