d3e22dfc30e73c0ca254982694e9704104a3ac44
ozfrfr <hosannacchigbu@gmail.com>
2023-11-07 11:39:18 -0800

    cat_file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    with open(filename, "r") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         content = file.read()
         print(content)
