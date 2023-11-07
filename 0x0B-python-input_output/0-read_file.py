1953fef279cd371f98de302c5ec30d8d0482308f
ozfrfr <hosannacchigbu@gmail.com>
2023-11-07 11:56:43 -0800

    cat_file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,8 +2,4 @@
 
 def read_file(filename=""):
     with open(filename, "r", encoding="utf-8") as file:
-        content = file.read()
-        print(content)
-
-if __name__ == "__main__":
-    pass
+    print(file.read(), end="")
